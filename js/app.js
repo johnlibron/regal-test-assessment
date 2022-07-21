d0388d146f8bbdfda1349e880719076d31f45b34
John James Libron <johnjames.libron@gmail.com>
2022-07-22 00:39:16 +0800

    Add documentations

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
+/**
+ * @author      John James Libron <johnjames.libron@gmail.com>
+ * @version     1.0
+ * @date        22 Jul 2022
+ * @description The app.js implements the functions for user interface in index.html file. 
+ *              I used jQuery <https://jquery.com/> to handle the input event 
+ *              handler functions and creating new elements/html content.
+ */
 'use strict';
 $(document).ready(function() {
 	app.init();
 });
 var app = {
+  /**
+   * Instantiate all call event handlers in the application.
+   * For location search input, list group will be empty if 
+   *    input is also empty, otherwise it will populate the cities 
+   *    on the list group based on the autocomplete of the search text.
+   * For the list group, it will display the daily forecast of the selected city location.
+   */
   init: function() {
     $("#location-search").keyup(function(e) {
       if (e.target.value === '') {
@@ -15,6 +30,10 @@ var app = {
       $("#location-search").val(e.target.innerText);
     });
   },
+  /**
+   * Only top 5 cities based on the autocomplete of the search text will populate in the list group.
+   * @param data  Response of forecast.getLocationSearch function
+   */
   populateListGroup: function(data) {
     let newData = data.slice(0,5); // remove items if index > 5
     $.each(newData, function(key, value) {
@@ -26,6 +45,10 @@ var app = {
       );
     });
   },
+  /**
+   * Only the next 3 days of daily forecasts for a specific location will populate in the card list.
+   * @param data  Response of forecast.getDailyForecast function
+   */
   show3DailyForecasts: function(data) {
     let newData = data.slice(0,3); // remove items if index > 3
     $.each(newData, function(key, value) {
@@ -42,20 +65,36 @@ var app = {
       );
     });
   },
+  /**
+   * Create new element for Daily Forecast date
+   * @param date  DailyForecasts.Date
+   */
   createElementForecastDate: function(date) {
     var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const newDate = new Date(date).toLocaleDateString("en-US", options);
     return $("<div/>").addClass("col-sm-12 col-md-12 col-lg-3 forecast-date").text(newDate);
   },
+  /**
+   * Create new element for Daily Forecast day icon
+   * @param icon  DailyForecasts.Day.Icon
+   */
   createElementDayIcon: function(icon) {
     return $("<div/>").addClass("col-sm-12 col-md-12 col-lg-2 forecast-icon")
             .append( $("<img/>").attr("src", `../regal-test-assessment/assets/icons/${icon}.png`)
             .addClass("card-img")
     )
   },
+  /**
+   * Create new element for Daily Forecast phrase
+   * @param phrase  DailyForecasts.Day.IconPhrase
+   */
   createElementPhrase: function(phrase) {
     return $("<div/>").addClass("col-sm-12 col-md-12 col-lg-4 phrase").text(phrase);
   },
+  /**
+   * Create new element for Daily Forecast temperature
+   * @param temperature  DailyForecasts.Temperature
+   */
   createElementTemperature: function(temperature) {
     const maxTemp = app.convertFahrenheitToCelsius(temperature.Maximum.Value);
     const minTemp = app.convertFahrenheitToCelsius(temperature.Minimum.Value);
@@ -63,7 +102,11 @@ var app = {
             .append( $("<span/>").addClass("max-temp").text(`${maxTemp}\xB0`) )
             .append( $("<span/>").addClass("min-temp").text(`/${minTemp}\xB0`) );
   },
+  /**
+   * Convert Fahrenheit value to Celsius
+   * @param value  temperature
+   */
   convertFahrenheitToCelsius: function(value) {
-    return Math.floor((value - 32) * 5 / 9)
+    return Math.floor((value - 32) * 5 / 9);
   }
 };
--- js/forecast.js
+++ js/forecast.js
@@ -1,7 +1,24 @@
+/**
+ * @author      John James Libron <johnjames.libron@gmail.com>
+ * @version     1.0
+ * @date        22 Jul 2022
+ * @description The forecast.js requests to AccuWeather Dataservice to get data 
+ *              from Locations API and Forecast API. 
+ *              I used jQuery AJAX <https://api.jquery.com/jquery.ajax/> to perform 
+ *              async http requests to the AccuWeather API <https://developer.accuweather.com/>.
+ *              Need to replace the forecast.key to a new API key because 
+ *              AccuWeather puts a limit on queries on a free account.
+ */
 'use strict';
 var forecast = {
   url: "https://dataservice.accuweather.com",
   key: "iF9XO7x4yIqfEpNxKkig0qBf21ZEAoDA",
+  /**
+   * Returns basic information about locations matching an autocomplete of the search text.
+   * If the request is success, it will populate the cities in the list group,
+   *    otherwise it will print the error message in the console log.
+   * @param value  Input value of location-search
+   */
   getLocationSearch: function(value) {
     $.ajax({
       url: `${forecast.url}/locations/v1/cities/autocomplete`,
@@ -19,6 +36,12 @@ var forecast = {
       }
     });
   },
+  /**
+   * Returns an array of daily forecasts for the next 5 days for a specific location. 
+   * If the request is success, it will show the next 3 days of the daily forecast in the card list,
+   *    otherwise it will print the error message in the console log.
+   * @param locationKey  getLocationSearch.data.Key
+   */
   getDailyForecast: function(locationKey) {
     $.ajax({
       url: `${forecast.url}/forecasts/v1/daily/5day/${locationKey}`,
